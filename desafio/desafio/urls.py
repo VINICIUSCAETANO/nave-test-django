e5646af32d812bd1acffed2787f3518f0ca02dcb
VINICIUSCAETANO <viniciusbeschcaetano@gmail.com>
2020-08-09 22:53:37 -0300

    Adicionadas rotas GET e POST de Usuários

--- desafio/desafio/urls.py
+++ desafio/desafio/urls.py
@@ -13,13 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from nave import views
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from rest_framework_jwt.views import obtain_jwt_token
 from rest_framework.routers import url
 
 
 urlpatterns = [
+    path(r'users/', views.get_users),
     path('admin/', admin.site.urls),
     url(r'^api-token-auth/', obtain_jwt_token),
 ]
--- desafio/nave/views.py
+++ desafio/nave/views.py
@@ -1,4 +1,36 @@
-from django.shortcuts import render, redirect, get_object_or_404
+import json
+from django.core.exceptions import ObjectDoesNotExist
+
 
 from .models import User, Naver, Project
+from .serializers import UserSerializer, NaverSerializer, ProjectSerializer
+
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+
+@api_view(["GET"])
+@csrf_exempt
+def get_users(request):
+    users = User.objects.get(User.objects.all)
+    serializer = UserSerializer(users, many=True)
+    return JsonResponse({'users': serializer.data}, safe=False, status=status.HTTP_200_OK)    
+
+@api_view(["POST"])
+@csrf_exempt
+def add_user(request):
+    payload = json.loads(request.body)
+    try:
+        user = User.objects.create(
+            username = payload['email'],
+            password = payload['password']
+        )
+
+        serializer = UserSerializer(user)
+        return JsonResponse({'user': serializer.data}, status=status.HTTP_201_CREATED)
+    except Exception:
+        return JsonResponse({'error': 'Erro do Servidor, tente mais tarde'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
